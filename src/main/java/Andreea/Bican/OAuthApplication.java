5fad094db6fe8ab0dfe19e295d5afa25d24606d0
andreabican <andreeabican@yahoo.com>
2016-05-13 00:42:28 +0300

    Added html page

---
+++ src/main/java/Andreea/Bican/OAuthApplication.java
@@ -0,0 +1,12 @@
+package Andreea.Bican;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthApplication.class, args);
+	}
+}
