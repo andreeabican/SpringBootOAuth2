fd4efdd6ab9403978a8e6b5171cddb21c573fd9f
andreabican <andreeabican@yahoo.com>
2016-05-13 01:51:26 +0300

    Added method to describe the currently authenticated user

--- src/main/java/Andreea/Bican/OAuthApplication.java
+++ src/main/java/Andreea/Bican/OAuthApplication.java
@@ -2,11 +2,38 @@ package Andreea.Bican;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+/* Annotation needed for connecting with Facebook*/
+@EnableOAuth2Sso
 @SpringBootApplication
-public class OAuthApplication {
+@RestController
+public class OAuthApplication extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OAuthApplication.class, args);
 	}
+
 }
