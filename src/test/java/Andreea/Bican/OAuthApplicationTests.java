7fc5c99416abd51a150a580cf6d8152f1508c23d
andreabican <andreeabican@yahoo.com>
2016-05-17 22:09:43 +0300

    Modified the context loader

--- src/test/java/Andreea/Bican/OAuthApplicationTests.java
+++ src/test/java/Andreea/Bican/OAuthApplicationTests.java
@@ -3,11 +3,14 @@ package Andreea.Bican;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = OAuthApplication.class)
+@ContextConfiguration(loader=AnnotationConfigContextLoader.class)
 @WebAppConfiguration
 public class OAuthApplicationTests {
 
