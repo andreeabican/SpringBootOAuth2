8b7354f89a9c02a1dcbe772d9c8b738819964406
andreabican <andreeabican@yahoo.com>
2016-06-18 03:05:01 +0300

    Updated

--- security/src/main/java/Andreea/Bican/impl/Oauth2/FacebookAuthentication/FacebookFilter.java
+++ security/src/main/java/Andreea/Bican/impl/Oauth2/FacebookAuthentication/FacebookFilter.java
@@ -1,52 +1,60 @@
 package Andreea.Bican.impl.Oauth2.FacebookAuthentication;
 
-import Andreea.Bican.impl.Configuration.CustomUserInfoTokenServices;
-import Andreea.Bican.impl.Configuration.SecurityConfiguration;
-import Andreea.Bican.impl.Oauth2.ProviderFilter;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.oauth2.common.AuthenticationScheme.form;
+import static org.springframework.security.oauth2.common.AuthenticationScheme.query;
+
 /**
  * Created by andre on 13.06.2016.
  */
 
+public class FacebookFilter{
 
-public class FacebookFilter extends ProviderFilter {
-
-    OAuth2ClientContext oAuth2ClientContext;
-
-    public FacebookFilter()
+    public List<String> createScopesList()
     {
-        oAuth2ClientContext = SecurityConfiguration.oauth2ClientContext;
+        List<String> scopes = new ArrayList<>();
+        scopes.add("email");
+        return scopes;
     }
 
-    public String createAuthenticationPath(String provider)
-    {
-        return "/login/" + provider;
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix="facebook.client")
     OAuth2ProtectedResourceDetails facebook() {
-        return new AuthorizationCodeResourceDetails();
+       AuthorizationCodeResourceDetails authorizationCodeResourceDetails = new AuthorizationCodeResourceDetails();
+
+        authorizationCodeResourceDetails.setClientId("clientId");
+        authorizationCodeResourceDetails.setClientSecret("clientSecret");
+        authorizationCodeResourceDetails.setAccessTokenUri("https://graph.facebook.com/oauth/access_token");
+        authorizationCodeResourceDetails.setUserAuthorizationUri("https://www.facebook.com/dialog/oauth");
+        authorizationCodeResourceDetails.setTokenName("oauth_token");
+        authorizationCodeResourceDetails.setAuthenticationScheme(query);
+        authorizationCodeResourceDetails.setClientAuthenticationScheme(form);
+        authorizationCodeResourceDetails.setScope(createScopesList());
+
+        return authorizationCodeResourceDetails;
     }
 
-    @Bean
-    @ConfigurationProperties(prefix="facebook.resource")
-    ResourceServerProperties facebookResource() { return new ResourceServerProperties(); }
+    ResourceServerProperties facebookResource() {
+        ResourceServerProperties resourceServerProperties = new ResourceServerProperties();
+        resourceServerProperties.setUserInfoUri("https://graph.facebook.com/me?fields=id,name,email");
 
-    public OAuth2ClientAuthenticationProcessingFilter createFilter() {
+        return resourceServerProperties;
+    }
 
-        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(createAuthenticationPath("facebook"));
+    public OAuth2ClientAuthenticationProcessingFilter createFilter(OAuth2ClientContext oAuth2ClientContext) {
+
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
         OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oAuth2ClientContext);
         facebookFilter.setRestTemplate(facebookTemplate);
-        facebookFilter.setTokenServices(new CustomUserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId(), "facebook"));
+        facebookFilter.setTokenServices(new FacebookCustomUserInfoTokenService(facebookResource().getUserInfoUri(), facebook().getClientId()));
+
         return facebookFilter;
     }
 }
