5dd6c058a8dc9cef5795a6a5cf7ac889fa3442dc
andreabican <andreeabican@yahoo.com>
2016-06-18 03:05:42 +0300

    Class that parses the token from Google

--- security/src/main/java/Andreea/Bican/impl/Oauth2/GoogleAuthentication/GoogleCustomUserInfoTokenService.java
+++ security/src/main/java/Andreea/Bican/impl/Oauth2/GoogleAuthentication/GoogleCustomUserInfoTokenService.java
@@ -1,7 +1,79 @@
 package Andreea.Bican.impl.Oauth2.GoogleAuthentication;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by andre on 13.06.2016.
  */
-public class GoogleCustomUserInfoTokenService {
+public class GoogleCustomUserInfoTokenService extends UserInfoTokenServices {
+
+    public GoogleCustomUserInfoTokenService(String userInfoEndpointUrl, String clientId)
+    {
+        super(userInfoEndpointUrl, clientId);
+    }
+
+    public OAuth2Authentication loadAuthentication(String accessToken) throws AuthenticationException
+    {
+        OAuth2Authentication auth = super.loadAuthentication(accessToken);
+        if (userIsKnown(getUserDetails(auth))) {
+            getUserDetails(auth).put("userId", getUserId(getUserDetails(auth)));
+            getUserDetails(auth).put("userEmail", getUserEmail(getUserDetails(auth)));
+            getUserDetails(auth).put("userName", getUserName(getUserDetails(auth)));
+        }
+        else {
+            throw new UsernameNotFoundException("Unknown user: " + getUserEmail(getUserDetails(auth)));
+        }
+
+        return auth;
+    }
+
+    private boolean userIsKnown(Map<String, Object> userDetails)
+    {
+        String email = getUserEmail(userDetails);
+        if (email.equals("unknown")) return false;
+        // look in db here
+        return true;
+    }
+
+    private String getUserName(Map<String, Object> userDetails)
+    {
+        if(userDetails.containsKey("displayName")){
+            return (String) userDetails.get("displayName");
+        }
+        return "unknown";
+    }
+
+    private String getUserEmail(Map<String, Object> userDetails)
+    {
+        if (userDetails.containsKey("emails")) {
+            List<Map<String, Object>> emails =
+                    (List<Map<String, Object>>) userDetails.get("emails");
+            for (Map<String, Object> emailAccount : emails) {
+                if (emailAccount.containsKey("value")) {
+                    return (String)emailAccount.get("value");
+                }
+            }
+        }
+        return "unknown";
+    }
+
+    private String getUserId(Map<String, Object> userDetails)
+    {
+        if(userDetails.containsKey("id"))
+        {
+            return (String)userDetails.get("id");
+        }
+        return "unknown";
+    }
+
+    private Map<String, Object> getUserDetails(OAuth2Authentication auth)
+    {
+        return (Map<String, Object>) auth.getUserAuthentication().getDetails();
+    }
 }
