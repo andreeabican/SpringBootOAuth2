a529bc8d8d96cb63fc1962500eb99fa234a5e61b
andreabican <andreeabican@yahoo.com>
2016-06-21 16:41:21 +0300

    Stored client access token

--- model/src/main/java/Andreea/Bican/CurrentUser.java
+++ model/src/main/java/Andreea/Bican/CurrentUser.java
@@ -4,14 +4,30 @@ package Andreea.Bican;
  * Created by andre on 27.05.2016.
  */
 public class CurrentUser{
+
     private static User currentUser;
-    public static String accessToken;
 
-    public static User getUser() { return currentUser;}
+    private static String accessToken;
 
-    public static void login(User user){ currentUser = user; }
+    private static String provider;
+
+    private static String userName;
+
+    public void login(){  currentUser = new User();  }
 
     public static void logout(){ currentUser = null; }
+
+    public static void setUserName(String username){ userName = username;  }
+
+    public static String getUserName(){ return userName;  }
+
+    public static void setAccessToken(String token){ accessToken = token;}
+
+    public static String getAccessToken(){ return accessToken; }
+
+    public static void setProvider(String authProvider){ provider = authProvider; }
+
+    public static String getProvider(){ return provider; }
 
     public static boolean isAuthenticated(){
         if(currentUser != null) {
