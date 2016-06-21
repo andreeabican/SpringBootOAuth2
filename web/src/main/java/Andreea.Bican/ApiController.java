53bbaa9b710e8704792b9148242fec3787432f3a
andreabican <andreeabican@yahoo.com>
2016-06-21 16:43:51 +0300

    Separated views

--- web/src/main/java/Andreea.Bican/ApiController.java
+++ web/src/main/java/Andreea.Bican/ApiController.java
@@ -1,12 +1,7 @@
 package Andreea.Bican;
 
-import com.sun.javafx.geom.transform.Identity;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -18,65 +13,15 @@ import java.security.Principal;
 @RestController
 public class ApiController
 {
-    UserRepository userRepository = new UserRepository();
 
     @RequestMapping("/user")
-    public Principal user(Principal identity) throws IOException, ParseException {
-        if(identity == null) {
+    public Principal user(Principal principal) throws IOException, ParseException {
+        if(principal == null) {
+            CurrentUser.logout();
             return null;
         }else{
-            return identity;
+            return principal;
         }
     }
 
-    @RequestMapping(value="/danceclasses", method = RequestMethod.GET)
-    public String danceClasses(@RequestHeader("token")String token) {
-            if(token.equals(CurrentUser.accessToken))
-                return token;
-        if(CurrentUser.isAuthenticated()){
-            if(CurrentUser.getUser().getAuthority()){
-                return "Welcome to your dance class";
-            }else{
-                return "You currently don't have a dance class";
-            }
-        }else{
-            return "You need to authenticate";
-        }
-    }
-
-
-    public User getLoggedUser(Identity identity) throws IOException, ParseException {
-
-        JSONParser parser = new JSONParser();
-        User user = new User();
-        try{
-            JSONObject jsonObject = (JSONObject)parser.parse(identity.toString());
-
-            JSONObject userAuthJSON = (JSONObject)jsonObject.get("userAuthentication");
-            JSONObject detailsJSON = (JSONObject)userAuthJSON.get("details");
-            String userName = (String)detailsJSON.get("userName");
-            user.setUserName(userName);
-
-            String email = (String) detailsJSON.get("userEmail");
-            user.setEmail(email);
-
-            String id = (String) detailsJSON.get("userId");
-            user.setId(id);
-            return user;
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @RequestMapping("/greeting")
-    public String greeting() {
-        if(CurrentUser.isAuthenticated()) {
-            return "Hello user " + CurrentUser.getUser().getUserName() + ".\n"
-                    + "Your email is: " + CurrentUser.getUser().getEmail() + ".\n"
-                    + "Your UUID is " + CurrentUser.getUser().getId();
-        }else{
-            return "Hello, annonymous";
-        }
-    }
 }
