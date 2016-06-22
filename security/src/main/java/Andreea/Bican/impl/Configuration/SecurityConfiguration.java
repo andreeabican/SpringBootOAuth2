7b04cb84063bd54c2bc43997237a005478cc13a5
andreabican <andreeabican@yahoo.com>
2016-06-22 19:29:01 +0300

    Pulled out methods for filter creation

--- security/src/main/java/Andreea/Bican/impl/Configuration/SecurityConfiguration.java
+++ security/src/main/java/Andreea/Bican/impl/Configuration/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package Andreea.Bican.impl.Configuration;
 
+import Andreea.Bican.impl.Oauth2.Filters.CSFRHeaderFilter;
 import Andreea.Bican.impl.Oauth2.FacebookAuthentication.FacebookFilter;
 import Andreea.Bican.impl.Oauth2.GoogleAuthentication.GoogleFilter;
+import Andreea.Bican.impl.Oauth2.Filters.ProviderFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -11,31 +13,19 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.filter.CompositeFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Configuration
-@Import({FacebookFilter.class, GoogleFilter.class})
+@Import({FacebookFilter.class, GoogleFilter.class, ProviderFilter.class, CSFRHeaderFilter.class})
 @EnableOAuth2Client
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -43,12 +33,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     OAuth2ClientContext oauth2ClientContext;
 
     @Autowired
-    @Qualifier("facebookFilter")
-    OAuth2ClientAuthenticationProcessingFilter facebookFilter;
+    @Qualifier("csrfHeaderFilter")
+    Filter csrfHeaderFilter;
 
     @Autowired
-    @Qualifier("googleFilter")
-    OAuth2ClientAuthenticationProcessingFilter googleFilter;
+    @Qualifier("providerFilter")
+    CompositeFilter compositeFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -64,45 +54,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                      .csrf().csrfTokenRepository(csrfTokenRepository())
                 .and()
-                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
-                    .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-    }
-
-    private Filter csrfHeaderFilter() {
-        return new OncePerRequestFilter() {
-
-            @Override
-            protected void doFilterInternal(HttpServletRequest request,
-                                            HttpServletResponse response, FilterChain filterChain)
-                    throws ServletException, IOException {
-                CsrfToken csrf = (CsrfToken) request
-                        .getAttribute(CsrfToken.class.getName());
-                if (csrf != null) {
-                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-                    String token = csrf.getToken();
-                    if (cookie == null
-                            || token != null
-                            && !token.equals(cookie.getValue())) {
-                        cookie = new Cookie("XSRF-TOKEN", token);
-                        cookie.setPath("/");
-                        response.addCookie(cookie);
-                    }
-                }
-                filterChain.doFilter(request, response);
-            }
-        };
-    }
-
-    /* Redirect from the app to Facebook and Google+*/
-    private Filter ssoFilter() {
-        CompositeFilter filter = new CompositeFilter();
-        List<Filter> filters = new ArrayList<>();
-
-        filters.add(facebookFilter);
-        filters.add(googleFilter);
-
-        filter.setFilters(filters);
-        return filter;
+                    .addFilterAfter(csrfHeaderFilter, CsrfFilter.class)
+                    .addFilterBefore(compositeFilter, BasicAuthenticationFilter.class);
     }
 
     @Bean
