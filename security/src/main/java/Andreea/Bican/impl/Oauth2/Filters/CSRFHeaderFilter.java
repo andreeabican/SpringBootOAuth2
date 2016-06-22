76c008f201cea3d5d783c73c68af85887a9e1705
andreabican <andreeabican@yahoo.com>
2016-06-22 19:34:44 +0300

    Added class which for the CSRFFilter

---
+++ security/src/main/java/Andreea/Bican/impl/Oauth2/Filters/CSRFHeaderFilter.java
@@ -0,0 +1,47 @@
+package Andreea.Bican.impl.Oauth2.Filters;
+
+import Andreea.Bican.impl.IFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by andre on 22.06.2016.
+ */
+public class CSRFHeaderFilter implements IFilter{
+
+    @Bean(name="csfrHeaderFilter")
+    public Filter createFilter() {
+        return new OncePerRequestFilter() {
+
+            @Override
+            protected void doFilterInternal(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain filterChain)
+                    throws ServletException, IOException {
+                CsrfToken csrf = (CsrfToken) request
+                        .getAttribute(CsrfToken.class.getName());
+                if (csrf != null) {
+                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                    String token = csrf.getToken();
+                    if (cookie == null
+                            || token != null
+                            && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");
+                        response.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+}
