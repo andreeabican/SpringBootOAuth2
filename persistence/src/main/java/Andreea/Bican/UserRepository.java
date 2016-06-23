3270dc44fcd188c8b91f8aa9854b45edec55256c
andreabican <andreeabican@yahoo.com>
2016-06-23 15:42:31 +0300

    Updated interface

--- persistence/src/main/java/Andreea/Bican/UserRepository.java
+++ persistence/src/main/java/Andreea/Bican/UserRepository.java
@@ -6,26 +6,6 @@ import java.util.List;
 /**
  * Created by andre on 27.05.2016.
  */
-public class UserRepository{
-    List<User> listOfUsers;
-
-    public User getUser(String userId) {
-        for(User user : listOfUsers){
-            if(userId.equals(user.getId())){
-                return user;
-            }
-        }
-        return null;
-    }
-
-    public UserRepository(){
-        listOfUsers = new LinkedList<>();
-        User hardcodedUser = new User();
-        hardcodedUser.setEmail("andreeasanzianabican@gmail.com");
-        hardcodedUser.setId("110493580298130411508");
-        hardcodedUser.setUserName("Andreea Sanziana Bican");
-        hardcodedUser.setAuthority(true);
-
-        listOfUsers.add(hardcodedUser);
-    }
+public interface UserRepository {
+    public User getUser(String userId);
 }
