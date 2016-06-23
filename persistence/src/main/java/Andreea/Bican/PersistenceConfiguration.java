4efbc2ad420653b014937fccabf01c19bd5ac85d
andreabican <andreeabican@yahoo.com>
2016-06-23 15:56:49 +0300

    Added spring-injected service, repository, and model modules/packages

---
+++ persistence/src/main/java/Andreea/Bican/PersistenceConfiguration.java
@@ -0,0 +1,12 @@
+package Andreea.Bican;
+
+/**
+ * Created by andre on 23.06.2016.
+ */
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@Configuration
+public class PersistenceConfiguration {
+
+}
