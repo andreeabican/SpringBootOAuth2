a7d8639310b9d3cfd757b722fe663f564b692e1f
andreabican <andreeabican@yahoo.com>
2016-06-23 15:39:33 +0300

    Deleted getters and setters for username, email, token and provider

--- model/src/main/java/Andreea/Bican/CurrentUser.java
+++ model/src/main/java/Andreea/Bican/CurrentUser.java
@@ -7,27 +7,13 @@ public class CurrentUser{
 
     private static User currentUser;
 
-    private static String accessToken;
-
-    private static String provider;
-
-    private static String userName;
-
     public void login(){  currentUser = new User();  }
 
+    public static void setUser(User user){currentUser = user; }
+
+    public static User getUser(){ return currentUser;  }
+
     public static void logout(){ currentUser = null; }
-
-    public static void setUserName(String username){ userName = username;  }
-
-    public static String getUserName(){ return userName;  }
-
-    public static void setAccessToken(String token){ accessToken = token;}
-
-    public static String getAccessToken(){ return accessToken; }
-
-    public static void setProvider(String authProvider){ provider = authProvider; }
-
-    public static String getProvider(){ return provider; }
 
     public static boolean isAuthenticated(){
         if(currentUser != null) {
