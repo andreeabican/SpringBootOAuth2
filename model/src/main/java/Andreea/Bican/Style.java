e92973bac115344aa497a4c5dde12074a0e67d0e
andreabican <andreeabican@yahoo.com>
2016-06-23 15:50:27 +0300

    Added style model

---
+++ model/src/main/java/Andreea/Bican/Style.java
@@ -0,0 +1,28 @@
+package Andreea.Bican;
+
+/**
+ * Created by andre on 23.06.2016.
+ */
+public class Style {
+    private int id;
+    private String name;
+
+    public Style() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
