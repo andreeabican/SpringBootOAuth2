ad5be8171d04f131ce28a7f4696dc52b3c56e169
andreabican <andreeabican@yahoo.com>
2016-06-23 15:48:16 +0300

    Added authorities class

---
+++ model/src/main/java/Andreea/Bican/Authorities.java
@@ -0,0 +1,38 @@
+package Andreea.Bican;
+
+/**
+ * Created by andre on 23.06.2016.
+ */
+public class Authorities {
+    private int pk;
+    private String uuid;
+    private String authority;
+
+    public Authorities() {
+    }
+
+    public int getPk() {
+        return pk;
+    }
+
+    public void setPk(int pk) {
+        this.pk = pk;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+}
