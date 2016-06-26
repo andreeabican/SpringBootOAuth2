84e11b052e6a440597669855d129815e7d9093d4
andreabican <andreeabican@yahoo.com>
2016-06-27 00:01:36 +0300

    Updated

--- model/src/main/java/Andreea/Bican/CurrentUser.java
+++ model/src/main/java/Andreea/Bican/CurrentUser.java
@@ -7,13 +7,10 @@ public class CurrentUser{
 
     private static User currentUser;
 
-    public void login(){  currentUser = new User();  }
 
     public static void setUser(User user){currentUser = user; }
 
     public static User getUser(){ return currentUser;  }
-
-    public static void logout(){ currentUser = null; }
 
     public static boolean isAuthenticated(){
         if(currentUser != null) {
