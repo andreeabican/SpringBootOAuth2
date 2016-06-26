f0f2a364f3c2cfeab7a0bd5dbafab64a8a081122
andreabican <andreeabican@yahoo.com>
2016-06-27 00:38:04 +0300

    Check if the token is assigned to any user

--- security/src/main/java/Andreea/Bican/impl/Configuration/ProviderAccessToken.java
+++ security/src/main/java/Andreea/Bican/impl/Configuration/ProviderAccessToken.java
@@ -1,24 +1,33 @@
 package Andreea.Bican.impl.Configuration;
 
-import Andreea.Bican.CurrentUser;
+import Andreea.Bican.User;
 import Andreea.Bican.impl.IAccessToken;
 import Andreea.Bican.impl.Oauth2.FacebookAuthentication.FacebookAccessToken;
 import Andreea.Bican.impl.Oauth2.GoogleAuthentication.GoogleAccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.HashMap;
 
 /**
  * Created by andre on 24.06.2016.
  */
 public class ProviderAccessToken {
 
-    public IAccessToken getProviderAccessToken()
+    @Autowired
+    @Qualifier("listOfUsers")
+    HashMap<String, User> loggedUsersList;
+
+    public IAccessToken getProviderAccessToken(String token)
     {
-        if(CurrentUser.getUser() == null){
-            return null;
+        if(loggedUsersList.containsKey(token)){
+            String provider = loggedUsersList.get(token).getProvider();
+            if(provider.equals("Facebook")){
+                return new FacebookAccessToken();
+            }else if(provider.equals("Google")){
+                return new GoogleAccessToken();
+            }
         }
-        if(CurrentUser.getUser().getProvider().equals("Facebook")){
-            return new FacebookAccessToken();
-        }else{
-            return new GoogleAccessToken();
-        }
+        return null;
     }
 }
