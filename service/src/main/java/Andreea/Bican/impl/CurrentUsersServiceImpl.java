64b01acddce3646e73c1ea366f700b9d45e7e4a5
andreabican <andreeabican@yahoo.com>
2016-06-28 17:00:41 +0300

    Updated

--- service/src/main/java/Andreea/Bican/impl/CurrentUsersServiceImpl.java
+++ service/src/main/java/Andreea/Bican/impl/CurrentUsersServiceImpl.java
@@ -1,5 +1,6 @@
 package Andreea.Bican.impl;
 
+import Andreea.Bican.CurrentContextService;
 import Andreea.Bican.CurrentUsersService;
 import Andreea.Bican.User;
 import Andreea.Bican.UserRepository;
@@ -7,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by andre on 26.06.2016.
@@ -22,11 +25,26 @@ public class CurrentUsersServiceImpl implements CurrentUsersService{
     @Qualifier("listOfUsers")
     HashMap<String, User> loggedUsersList;
 
+    @Autowired
+    CurrentContextService securityCurrentContextService;
+
     @Override
     public void addLoggedUser(String token, String email) {
         User user = userRepository.getUser(email);
-        loggedUsersList.put(token, user);
+        User currentContextUser = securityCurrentContextService.getUserFromCurrentContext();
+
+        if(user!=null) {
+            user.setToken(currentContextUser.getToken());
+            user.setProvider(currentContextUser.getProvider());
+            user.setId(currentContextUser.getId());
+            loggedUsersList.put(token, user);
+        }else{
+            //Unknown user by system
+            currentContextUser.setAuthorities(new HashSet<String>(Arrays.asList("GUEST")));
+            loggedUsersList.put(token, currentContextUser);
+        }
     }
+
 
     @Override
     public User getLoggedUserByToken(String token) {
