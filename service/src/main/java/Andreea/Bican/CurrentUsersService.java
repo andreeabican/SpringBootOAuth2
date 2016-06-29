f64f4ad6be0883d294b1a552fa96721514ec3866
andreabican <andreeabican@yahoo.com>
2016-06-29 14:20:38 +0300

    Removed duplicated users from list

--- service/src/main/java/Andreea/Bican/CurrentUsersService.java
+++ service/src/main/java/Andreea/Bican/CurrentUsersService.java
@@ -5,8 +5,23 @@ package Andreea.Bican;
  */
 public interface CurrentUsersService {
 
-    void addLoggedUser(String token, String id);
+    void logUser(String token, String id);
+
     User getLoggedUserByToken(String token);
+
     boolean checkLoggedUser(String token);
-    void deleteLoggedUser(String token);
+
+    void deleteLoggedUserByToken(String token);
+
+    void deleteLoggedUserByEmail(String email);
+
+    void updateTokenForLoggedUser(String newToken, String email);
+
+    User getUserFromRepository(String email);
+
+    User getLoggedUserByEmail(String email);
+
+    void addUserFromRepository(String token, User user);
+
+    void addUserFromCurrentContext(String token);
 }
--- service/src/main/java/Andreea/Bican/impl/CurrentUsersServiceImpl.java
+++ service/src/main/java/Andreea/Bican/impl/CurrentUsersServiceImpl.java
@@ -8,9 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Created by andre on 26.06.2016.
@@ -29,29 +28,73 @@ public class CurrentUsersServiceImpl implements CurrentUsersService{
     CurrentContextService securityCurrentContextService;
 
     @Override
-    public void addLoggedUser(String token, String email) {
-        User user = userRepository.getUser(email);
-        User currentContextUser = securityCurrentContextService.getUserFromCurrentContext();
-
-        if(user!=null) {
-            user.setToken(currentContextUser.getToken());
-            user.setProvider(currentContextUser.getProvider());
-            user.setId(currentContextUser.getId());
-            loggedUsersList.put(token, user);
-        }else{
-            //Unknown user by system
-            currentContextUser.setAuthorities(new HashSet<String>(Arrays.asList("GUEST")));
-            loggedUsersList.put(token, currentContextUser);
+    public void logUser(String token, String email) {
+        User loggedUser = getLoggedUserByEmail(email);
+        if(loggedUser != null)      //There is no logged user with the current email
+        {
+            updateTokenForLoggedUser(token, email);
+        }else{ /* If there is no user logged with current email look in the database
+             and check if the email is there*/
+            User userFromRepository = getUserFromRepository(email);
+            if(userFromRepository != null){
+                addUserFromRepository(token, userFromRepository);
+            }else{
+                addUserFromCurrentContext(token);
+            }
         }
     }
 
+    @Override
+    public void addUserFromCurrentContext(String token){
+        User user = securityCurrentContextService.getUserFromCurrentContext();
+        loggedUsersList.put(token, user);
+    }
 
+    @Override
+    public void addUserFromRepository(String token, User user){
+        User currentContextUser = securityCurrentContextService.getUserFromCurrentContext();
+        /* There are information that need to be updated, like provider, id
+         Those are not provided by the repository*/
+        user.setId(currentContextUser.getId());
+        user.setProvider(currentContextUser.getProvider());
+        loggedUsersList.put(token, user);
+    }
     @Override
     public User getLoggedUserByToken(String token) {
         if(loggedUsersList.containsKey(token)){
             return loggedUsersList.get(token);
         }
         return null;
+    }
+
+    @Override
+    public User getLoggedUserByEmail(String email){
+        for(Map.Entry<String, User> entry : loggedUsersList.entrySet()){
+            if(entry.getValue().getEmail().equals(email)){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public User getUserFromRepository(String email){
+        User user = userRepository.getUser(email);
+        return user;
+    }
+
+    @Override
+    public void updateTokenForLoggedUser(String newToken, String email){
+        String lastToken = null;
+        for(Map.Entry<String, User> entry : loggedUsersList.entrySet()){
+            if(entry.getValue().getEmail().equals(email)){
+                lastToken = entry.getKey();
+            }
+        }
+        if(lastToken != null){
+            User user = loggedUsersList.remove(lastToken);
+            loggedUsersList.put(newToken, user);
+        }
     }
 
     @Override
@@ -63,9 +106,19 @@ public class CurrentUsersServiceImpl implements CurrentUsersService{
     }
 
     @Override
-    public void deleteLoggedUser(String token) {
+    public void deleteLoggedUserByEmail(String email){
+        String token = null;
+        for(Map.Entry<String, User> entry : loggedUsersList.entrySet()){
+            if(entry.getValue().getEmail().equals(email)){
+                token = entry.getKey();
+            }
+        }
+        if(token != null){
+            deleteLoggedUserByToken(token);
+        }
+    }
+    @Override
+    public void deleteLoggedUserByToken(String token) {
         loggedUsersList.remove(token);
     }
-
-
 }
