c0f69cc2be7f1c89049ae08454e0a8717ea10201
andreabican <andreeabican@yahoo.com>
2016-07-03 23:35:55 +0300

    Added method to get email out of access token

--- security/src/main/java/Andreea/Bican/impl/Oauth2/GoogleAuthentication/GoogleAccessToken.java
+++ security/src/main/java/Andreea/Bican/impl/Oauth2/GoogleAuthentication/GoogleAccessToken.java
@@ -1,13 +1,19 @@
 package Andreea.Bican.impl.Oauth2.GoogleAuthentication;
 
 import Andreea.Bican.impl.IAccessToken;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
  * Created by andre on 21.06.2016.
  */
+@Service
 public class GoogleAccessToken implements IAccessToken {
 
     private final String USER_AGENT = "Mozilla/5.0";
@@ -25,10 +31,23 @@ public class GoogleAccessToken implements IAccessToken {
         //add request header
         con.setRequestProperty("User-Agent", USER_AGENT);
 
+
         int responseCode = con.getResponseCode();
         String responseMessage = con.getResponseMessage();
+        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line+"\n");
+        }
+        br.close();
+
+        JSONParser parser = new JSONParser();
+        Object object = parser.parse(String.valueOf(sb));
+        JSONObject jsonObject = (JSONObject) object;
+        System.out.println("Email " + jsonObject.get("email"));
         System.out.println("\nSending 'GET' request to URL : " + URL);
-        System.out.println("Response Code : " + responseCode);
+        System.out.println("Response Code : " + responseMessage);
         return responseMessage;
     }
 
