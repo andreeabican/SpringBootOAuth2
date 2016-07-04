2b23fb4efc58b993f4fb4866c63cd6823e9f9e62
andreabican <andreeabican@yahoo.com>
2016-07-04 20:28:07 +0300

    Updated

--- web/src/main/java/Andreea.Bican/WebApiViews/TokenView.java
+++ web/src/main/java/Andreea.Bican/WebApiViews/TokenView.java
@@ -1,6 +1,6 @@
 package Andreea.Bican.WebApiViews;
 
-import Andreea.Bican.impl.Oauth2.GoogleAuthentication.GoogleAccessToken;
+import Andreea.Bican.CurrentUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TokenView {
 
-    @Autowired
-    GoogleAccessToken accessToken;
-
     private final String URL = "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=";
 
     @Autowired
     private Andreea.Bican.TokenService tokenService;
 
+
+    @Autowired
+    CurrentUsersService currentUsersService;
+
     @RequestMapping("/getGoogleAccessToken")
     public String token(@RequestHeader(value = "code")String code) throws Exception {
-        String token = tokenService.getAccessToken(code);
-        accessToken.sendGet(URL + token);
+        String token = tokenService.getGoogleAccessToken(code);
+        String email = tokenService.getEmailFromGoogleAccessToken(token);
+        currentUsersService.logUserForWebApiSessions(token, email, "Google");
         return token;
     }
 }
