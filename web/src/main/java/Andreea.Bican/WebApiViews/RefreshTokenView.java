c3badeb35aba97ede6d13286940411e533d8d4c3
andreabican <andreeabican@yahoo.com>
2016-07-08 16:20:40 +0300

    Added method for obtaining the Google refresh token

---
+++ web/src/main/java/Andreea.Bican/WebApiViews/RefreshTokenView.java
@@ -0,0 +1,28 @@
+package Andreea.Bican.WebApiViews;
+
+import Andreea.Bican.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by andre on 07.07.2016.
+ */
+@RestController
+public class RefreshTokenView {
+
+    @Autowired
+    TokenService tokenService;
+
+    @RequestMapping("/getGoogleRefreshToken")
+    public String token(@RequestHeader(value = "code")String code) throws Exception {
+
+        String refreshToken = tokenService.getGoogleRefreshToken(code);
+        String token = tokenService.getGoogleAccessTokenByRefreshToken(refreshToken);
+        String email = tokenService.getEmailFromGoogleAccessToken(token);
+        System.out.println("Token is " + token + " email is: " + email);
+
+        return  refreshToken;
+    }
+}
