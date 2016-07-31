91b52fcf979492f616a71bd49ba1d49df28b58f0
andreabican <andreeabican@yahoo.com>
2016-07-31 23:34:57 +0300

    Updated

--- app/src/main/java/Andreea/Bican/OAuthApplication.java
+++ app/src/main/java/Andreea/Bican/OAuthApplication.java
@@ -14,7 +14,6 @@ public class OAuthApplication
 
         SpringApplication.run(OAuthApplication.class, args);
 
-        System.out.println("This is the arg line " + args[0] + args[1] + args[2]);
         if (args.length > 2) {
             ClientAppDetails.setServiceAccountId(args[0]);
             ClientAppDetails.setServiceAccountUser(args[1]);
