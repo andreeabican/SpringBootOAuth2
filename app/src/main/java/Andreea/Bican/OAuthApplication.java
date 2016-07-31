f72b00ff43869b494b6e99ae60f0fa6dff30d244
andreabican <andreeabican@yahoo.com>
2016-07-31 23:12:42 +0300

    Parsed command line arguments

--- app/src/main/java/Andreea/Bican/OAuthApplication.java
+++ app/src/main/java/Andreea/Bican/OAuthApplication.java
@@ -3,9 +3,22 @@ package Andreea.Bican;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class OAuthApplication
 {
-    public static void main(String[] args){ SpringApplication.run(OAuthApplication.class, args); }
+    private static final String targetURL = "http://localhost:8181/loginGoogle";
 
+    public static void main(String[] args) throws IOException {
+
+        SpringApplication.run(OAuthApplication.class, args);
+
+        System.out.println("This is the arg line " + args[0] + args[1] + args[2]);
+        if (args.length > 2) {
+            ClientAppDetails.setServiceAccountId(args[0]);
+            ClientAppDetails.setServiceAccountUser(args[1]);
+            ClientAppDetails.setP12FileName(args[2]);
+        }
+    }
 }
