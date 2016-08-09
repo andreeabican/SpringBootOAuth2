7368f1297f0c0822e25567d673a6d2decacda399
andreabican <andreeabican@yahoo.com>
2016-08-09 18:56:42 +0300

    Updated

--- security/src/main/java/Andreea/Bican/impl/Configuration/SecurityConfiguration.java
+++ security/src/main/java/Andreea/Bican/impl/Configuration/SecurityConfiguration.java
@@ -47,6 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatcher("/**").authorizeRequests()
                 .antMatchers("/", "/login**", "/webjars/**").permitAll()
                 .antMatchers("/userAndSession/**").authenticated()
+                .antMatchers("/class").authenticated()
                 .and()
                     .logout()
                     .logoutSuccessUrl("/")
