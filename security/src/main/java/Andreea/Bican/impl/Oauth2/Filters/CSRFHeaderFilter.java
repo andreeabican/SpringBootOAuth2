ea6654424bcdd40b94abf5054291822cd8eb9912
andreabican <andreeabican@yahoo.com>
2016-08-25 18:22:20 +0300

    Added support for _csrf header in request

--- security/src/main/java/Andreea/Bican/impl/Oauth2/Filters/CSRFHeaderFilter.java
+++ security/src/main/java/Andreea/Bican/impl/Oauth2/Filters/CSRFHeaderFilter.java
@@ -28,16 +28,24 @@ public class CSRFHeaderFilter implements IFilter{
             protected void doFilterInternal(HttpServletRequest request,
                                             HttpServletResponse response, FilterChain filterChain)
                     throws ServletException, IOException {
-
+                CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+// Spring Security will allow the Token to be included in this header name
+                if(token != null) {
+                        response.setHeader("X-CSRF-HEADER", token.getHeaderName());
+// Spring Security will allow the token to be included in this parameter name
+                        response.setHeader("X-CSRF-PARAM", token.getParameterName());
+// this is the value of the token to be included as either a header or an HTTP parameter
+                        response.setHeader("X-CSRF-TOKEN", token.getToken());
+                }
                 CsrfToken csrf = (CsrfToken) request
                         .getAttribute(CsrfToken.class.getName());
                 if (csrf != null) {
                     Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-                    String token = csrf.getToken();
+                    String token2 = csrf.getToken();
                     if (cookie == null
-                            || token != null
-                            && !token.equals(cookie.getValue())) {
-                        cookie = new Cookie("XSRF-TOKEN", token);
+                            || token2 != null
+                            && !token2.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token2);
                         cookie.setPath("/");
                         response.addCookie(cookie);
                     }
