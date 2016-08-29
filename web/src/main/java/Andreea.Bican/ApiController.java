4fc89bee7b23628f4eabe577743eaf607164da33
andreabican <andreeabican@yahoo.com>
2016-08-30 01:18:43 +0300

    Extracted from Security Context Holder the Principal object

--- web/src/main/java/Andreea.Bican/ApiController.java
+++ web/src/main/java/Andreea.Bican/ApiController.java
@@ -1,11 +1,12 @@
 package Andreea.Bican;
 
 import org.json.simple.parser.ParseException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.security.Principal;
 
 /**
  * Created by grossb on 5/31/16.
@@ -13,8 +14,10 @@ import java.security.Principal;
 @RestController
 public class ApiController
 {
-    @RequestMapping("/user")
-    public Principal user(Principal principal) throws IOException, ParseException {
+    @RequestMapping(value="/user")
+    public String user() throws IOException, ParseException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String principal = (String) authentication.getPrincipal();
         return principal;
     }
 }
